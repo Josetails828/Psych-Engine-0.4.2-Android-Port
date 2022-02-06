eb7622949ed225abc77c6f6de252bbc6cb5c395a
Josepro12 <99135630+Josepro12@users.noreply.github.com>
2022-02-06 14:20:21 -0300

    Add files via upload

---
+++ assets/preload/data/thief/script.lua
@@ -0,0 +1,40 @@
+local allowCountdown = false
+function onStartCountdown()
+	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then -- Timer completed, play dialogue
+		startDialogue('dialogue', 'dimension');
+	end
+end
+
+-- Dialogue (When a dialogue is finished, it calls startCountdown again)
+function onNextDialogue(count)
+	-- triggered when the next dialogue line starts, 'line' starts with 1
+end
+
+function onSkipDialogue(count)
+	-- triggered when you press Enter and skip a dialogue line that was still being typed, dialogue line starts with 1
+end
+
+function opponentNoteHit()
+triggerEvent('Screen Shake', '0.001, 0.001', '0.001, 0.001');
+end
+
+function onUpdate(elapsed)
+	if curStep == 20 then
+		started = true
+	end
+songPos = getSongPosition()
+local currentBeat = (songPos/5000)*(curBpm/60)
+doTweenY('opponentmove', 'dad', 100 - 200*math.sin((currentBeat+12*12)*math.pi), 2)
+	end
+
